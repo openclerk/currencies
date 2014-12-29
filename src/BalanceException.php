e3b4a2bf90cbe73eed9e550203a4eb4b7a0860ce
Jevon Wright <jevon@jevon.org>
2014-12-30 00:21:13 +1300

    Introduce Monolog for logging
    BalanceableCurrencies can now throw BalanceExceptions if something
    bad happens

---
+++ src/BalanceException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Openclerk\Currencies;
+
+class BalanceException extends \Exception {
+  function __construct($message, \Exception $previous = null) {
+    parent::__construct($message, 0, $previous);
+  }
+}
--- src/BalanceableCurrency.php
+++ src/BalanceableCurrency.php
@@ -2,6 +2,8 @@
 
 namespace Openclerk\Currencies;
 
+use \Monolog\Logger;
+
 /**
  * An "BalanceableCurrency" is a {@link AddressableCurrency} where we can obtain balances for
  * a given account identifier.
@@ -11,7 +13,10 @@ interface BalanceableCurrency extends AddressableCurrency {
   /**
    * Get the account balance of the given account.
    * This method blocks until the balance has been obtained.
+   *
+   * @param $logger a logger to log info/error messages to
+   * @throws {@link BalanceException} if something happened and the balance could not be obtained
    */
-  public function getBalance($id);
+  public function getBalance($id, Logger $logger);
 
 }
