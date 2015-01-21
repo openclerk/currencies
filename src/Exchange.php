cf1e23d1f511d2de10b6f9558641837ba092626d
Jevon Wright <jevon@jevon.org>
2015-01-21 17:01:09 +1300

    Exchange::fetchAllRates() should also return currency pairs
    Issue #400

--- src/Exchange.php
+++ src/Exchange.php
@@ -37,10 +37,14 @@ interface Exchange {
 
   /**
    * Fetch all rates for a given currency, returns an array of
-   * (last_trade, bid, ask, volume).
+   * (last_trade, bid, ask, volume, currency1, currency2) as keyed by
+   * the concatenation of the two currencies together (and therefore should
+   * be six characters long).
+   *
    * Some fields may not be present for a given exchange.
    * `volume` is the last 24h volume.
-   * May also return additional fields.
+   *
+   * May also return additional fields, e.g. high/low/vwap
    *
    * @return A list of rates for the currency pair
    * @throws ExchangeRateException if there are no rates for the given pair
