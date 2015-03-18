149f4de48e2e432808bbcfdbd4ca704d2d53d8e3
Jevon Wright <jevon@jevon.org>
2015-03-18 14:44:40 +1300

    Add basic component-tests instance to check component

---
+++ test/CurrenciesComponentTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Openclerk\Currencies\Tests;
+
+class CurrenciesComponentTest extends \ComponentTests\ComponentTest {
+
+  function getRoots() {
+    return array(__DIR__ . "/..");
+  }
+
+}
